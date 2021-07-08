f342fc10542b2bf39249ea9265de690ded278de1
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-08 11:57:01 +0600

    updated auth0 config with use credentials

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-AUTH0_CONFIG = Rails.application.config_for(:auth0)
+AUTH0_CONFIG = Rails.application.credentials.auth0
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    AUTH0_CONFIG["auth0_client_id"],
-    AUTH0_CONFIG["auth0_client_secret"],
-    AUTH0_CONFIG["auth0_domain"],
+    AUTH0_CONFIG["client_id"],
+    AUTH0_CONFIG["client_secret"],
+    AUTH0_CONFIG["domain"],
     callback_path: "/auth/auth0/callback",
     authorize_params: {
       scope: "openid profile",
