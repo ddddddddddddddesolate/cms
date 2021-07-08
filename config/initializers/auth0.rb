a114dd56493050189549000c481ac24e93850327
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-08 15:49:45 +0600

    fixed auth0 config

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -5,10 +5,10 @@ AUTH0_CONFIG = Rails.application.credentials.auth0
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    AUTH0_CONFIG["client_id"],
-    AUTH0_CONFIG["client_secret"],
-    AUTH0_CONFIG["domain"],
-    callback_path: "/auth/auth0/callback",
+    AUTH0_CONFIG[:client_id],
+    AUTH0_CONFIG[:client_secret],
+    AUTH0_CONFIG[:domain],
+    callback_path: AUTH0_CONFIG[:callback_path],
     authorize_params: {
       scope: "openid profile",
     },
