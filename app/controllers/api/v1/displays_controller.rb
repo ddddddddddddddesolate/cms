971e8ac4748b9435c4855872f4b85fca2d29861a
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-09 15:19:17 +0600

    implemented displays update

--- app/controllers/api/v1/displays_controller.rb
+++ app/controllers/api/v1/displays_controller.rb
@@ -33,6 +33,20 @@ module Api
         end
       end
 
+      def update
+        result = Displays::UpdateDisplayService.call(current_user, display_params)
+
+        if result.success
+          render json: {
+                   data: result.display,
+                 }, status: :ok
+        else
+          render json: {
+                   errors: result.errors,
+                 }, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def display_params
---
+++ app/services/displays/update_display_service.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Displays
+  class UpdateDisplayService
+    include Callable
+
+    attr_reader :current_user, :display_params
+
+    def initialize(current_user, display_params)
+      @current_user = current_user
+      @display_params = display_params
+    end
+
+    def call
+      display = current_user.displays.find_by!(display_params.extract!(:id, :event_id))
+
+      OpenStruct.new(success: display.update(display_params), errors: display.errors, display: display)
+    end
+  end
+end
