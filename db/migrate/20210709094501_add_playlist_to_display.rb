7e6f07de58b13138c84681f94c09206e34967ed9
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-09 17:13:37 +0600

    allowed null for display playlist

--- db/migrate/20210709094501_add_playlist_to_display.rb
+++ db/migrate/20210709094501_add_playlist_to_display.rb
@@ -2,6 +2,6 @@
 
 class AddPlaylistToDisplay < ActiveRecord::Migration[6.1]
   def change
-    add_reference :displays, :playlist, null: false, foreign_key: true
+    add_reference :displays, :playlist, null: true, foreign_key: true
   end
 end
