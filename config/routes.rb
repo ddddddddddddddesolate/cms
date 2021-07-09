a81141ddb344c46e182208c0bd1079d6f5b63d0a
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-09 14:49:34 +0600

    added routes for displays

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
 
-      resources :events
+      resources :events do
+        resources :displays
+      end
     end
   end
 end
