d30a0a01f727b4f021c736169eb3ed561d7409ac
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-12 14:05:00 +0600

    added contents route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
       resources :playlists
+      resources :contents
       resources :events do
         resources :displays
       end
