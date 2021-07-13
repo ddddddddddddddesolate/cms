2d287c27422afafb28e0b5213458e6cc194d0ec0
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-13 13:12:57 +0600

    added seeder and created test files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(email: "example@purrweb.com", password: "123456", password_confirmation: "123456")
+files = Dir.glob(Rails.root.join("data", "*"))
+
+3.times do |e|
+  event = Event.create(name: "Event ##{e}", user_id: user.id)
+  3.times do |d|
+    playlist = Playlist.create(name: "Playlist for display ##{d}", duration: rand(3600..36000))
+    display = Display.create(name: "Display ##{d} for event ##{event.id}", event_id: event.id, playlist_id: playlist.id)
+    3.times do |s|
+      content = Content.create(content: File.open(files.sample))
+      slide = Slide.create(playlist_id: playlist.id, content_id: content.id, weight: s)
+    end
+  end
+end
