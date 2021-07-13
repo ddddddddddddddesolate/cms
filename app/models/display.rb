f261ee425073d4102ad7d4c94933208f7d96c73e
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-13 18:26:27 +0600

    added user association to display

--- app/models/display.rb
+++ app/models/display.rb
@@ -4,5 +4,7 @@ class Display < ApplicationRecord
   belongs_to :event
   belongs_to :playlist, optional: true
 
+  has_one :user, through: :event
+
   validates :name, presence: true, length: { maximum: 255 }, allow_blank: false
 end
