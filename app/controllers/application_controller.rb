7dcc12973c5ed60884a5155b4c9f29b44fe9ac92
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-13 18:26:09 +0600

    added custom response for not authorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,19 @@
 
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from Pundit::NotAuthorizedError, with: :render_not_authorized_error_response
 
   def render_not_found_response(message)
     render json: {
              error: message,
            }, status: :not_found
+  end
+
+  def render_not_authorized_error_response(message)
+    render json: {
+             error: message,
+           }, status: :forbidden
   end
 end
