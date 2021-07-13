902654abc81b90fa4e450b775121f196b7c66003
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-13 16:10:16 +0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
+      resources :contents
       resources :playlists do
-        resources :slides
-      end
-      resources :contents do
         resources :slides
       end
       resources :events do
