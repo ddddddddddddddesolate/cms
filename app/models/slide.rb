10312ada382fa845a82b8229dc1353370345f2de
Alexander <jtcubem_oduh_pa3@mail.ru>
2021-07-15 17:25:27 +0600

    removed default scope from slide

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -6,6 +6,4 @@ class Slide < ApplicationRecord
 
   validates :weight, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, presence: true, length: { maximum: 255 }, allow_blank: false
-
-  default_scope { order(weight: :asc) }
 end
